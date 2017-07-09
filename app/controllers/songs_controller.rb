5a511f66d806e3cacebca4b259ef5ed6109481ad
YOUR NAME <muvanlier@gmail.com>
2017-07-09 14:22:17 +0200

    added validations songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,34 @@
-class SongsController < ApplicationController
-  def index
-    @song = Song.all
+class songsController < ApplicationController
+
+    def index
+      @song = song.all
+    end
+
+    def new
+      @song = song.new
+    end
+
+    def show
+      @song = song.find(params[:id])
+    end
+
+    def create
+      song_params = params.require(:song).permit(:name, :song)
+
+      @song = song.new(song_params)
+
+      if @song.save
+         redirect_to @song
+      else
+         render 'new'
+      end
+    end
+
+    def destroy
+      @song = song.find(params[:id])
+
+      @song.destroy
+
+      redirect_to songs_path
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,10 @@
 class Song < ApplicationRecord
   belongs_to :artist
+
+
+  validates :name_song, presence: true
+  validates :name_artist, presence: true
+  validates :duration, presence: true
+  validates :genre, presence: true
+
 end
